e0802938c67b98eac8af39ad233d58216c5a327e
jacktarricone <54866783+jacktarricone@users.noreply.github.com>
2022-02-21 20:37:55 -0800

    updates

--- code/lab2.R
+++ code/lab2.R
@@ -97,7 +97,9 @@ colnames(g1) <- c("year","day","hour_min","sec","BattV_Min","PTemp_C","AirTC_Avg
 # tail(g1)
 
 # add in month column, switch months
-nday_jan <-9051
+nday_janv1 <-filter(g1, day <= 31)
+nday_jan <-as.integer(nrow(nday_janv1))
+
 month_jan <-rep(1, nday_jan) 
 month_feb <-rep(2, nrow(g1) - nday_jan)
 month <-c(month_jan, month_feb)
@@ -141,8 +143,6 @@ ggplot(g1) +
 
 
 
-
-
 ##########################
 ######## group 2 #########
 ##########################
@@ -155,12 +155,12 @@ g2_ascii <-readLines("/Users/jacktarricone/atms_748/data-code/lab2_data/Lab_2/Gr
 head(g2_ascii)
 
 # remove first unwanted col
-g2 <- g2[-c(1)]
+# g2 <- g2[-c(1)]
 head(g2)
 
 # rename columns using information in the ascii header from g2
-colnames(g2) <- c("year","day","hour_min","sec","BattV_Min","PTemp_C","AirTC_Avg",
-                  "RH","windSpeed","windDirection","gustWindSpeed","AirTemp")
+colnames(g2) <- c("year","day","hour_min","sec","BattV_Min","PTemp_C",
+                  "AirTC_Avg","RH","windSpeed","windDirection","gustWindSpeed","AirTemp")
 
 # turn day into dom
 # g2$day <-g2$day -28
@@ -168,7 +168,8 @@ colnames(g2) <- c("year","day","hour_min","sec","BattV_Min","PTemp_C","AirTC_Avg
 # tail(g2)
 
 # add in month column, switch months
-nday_jan <-9051
+nday_janv1 <-filter(g2, day <= 31)
+nday_jan <-as.integer(nrow(nday_janv1))
 month_jan <-rep(1, nday_jan) 
 month_feb <-rep(2, nrow(g2) - nday_jan)
 month <-c(month_jan, month_feb)
@@ -205,5 +206,149 @@ g2 <-filter(g2, date_time  >= "2022-01-25 17:05:00")
 
 ggplot(g2) +
   geom_line(aes(x = date_time, y = RH), size =.6)
+
+
+# write.csv(g2, "/Users/jacktarricone/atms_748/data-code/lab2_data/csvs/g2.csv")
+
+
+
+##########################
+######## group 4 #########
+##########################
+
+
+g4 <-read.csv("/Users/jacktarricone/atms_748/data-code/lab2_data/Lab_2/Group_4/Lab2/Data/CSV_21492.SlowResponse_2_2022_01_25_1651.dat",
+              header = TRUE)
+head(g4)
+
+g4_ascii <-readLines("/Users/jacktarricone/atms_748/data-code/lab2_data/Lab_2/Group_4/Lab2/Data/TOA5_21492.SlowResponse_2_2022_01_25_1651.dat") 
+head(g4_ascii)
+
+# remove first unwanted col
+g4 <- g4[-c(1)]
+head(g4)
+
+# rename columns using information in the ascii header from g4
+colnames(g4) <- c("year","day","hour_min","sec","AirTC",
+                  "RH","windSpeed","windDirection","gustWindSpeed","AirTemp")
+head(g4)
+
+# turn day into dom
+# g4$day <-g4$day -28
+# head(g4) # check, looks good
+# tail(g4)
+
+# add in month column, switch months
+nday_janv1 <-filter(g4, day <= 31)
+nday_jan <-as.integer(nrow(nday_janv1))
+month_jan <-rep(1, nday_jan) 
+month_feb <-rep(2, nrow(g4) - nday_jan)
+month <-c(month_jan, month_feb)
+g4 <-cbind(g4, month) # bind
+
+### add new day col
+# jan
+day_jan <-filter(g4, day <= 31)
+day_jan <-day_jan$day
+
+# feb
+day_feb <-filter(g4, day > 31)
+day_feb <-day_feb$day - 31
+
+# add col
+new_day <-c(day_jan, day_feb)
+tail(new_day)
+g4$new_day <-c(day_jan, day_feb)
+
+# create date col
+date <-mdy(paste0(g4$month,"-",g4$new_day,"-",g4$year))
+g4$date <-date # data to big dataframe
+
+#format time and paste, assuming all second values are 00
+time <- sprintf("%04d", as.numeric(as.character(g4$hour_min)))
+g4$time <-times(gsub("(..)(..)", "\\1:\\2:00", time))
+
+# combine into one date and time columb using lubridate
+g4$date_time <- ymd_hms(paste(g4$date, g4$time))
+head(g4)
+
+# filter off bad data in beginning
+# g4 <-filter(g4, date_time  >= "2022-01-25 17:05:00") 
+
+ggplot(g4) +
+  geom_line(aes(x = date_time, y = RH), size =.6)
+
+#write.csv(g4, "/Users/jacktarricone/atms_748/data-code/lab2_data/csvs/g4.csv")
+
+##########################
+######## group 5 #########
+##########################
+
+
+g5 <-read.csv("/Users/jacktarricone/atms_748/data-code/lab2_data/Lab_2/Group_5/CSV_21489.SlowResponse_3_2022_01_25_1719.dat",
+              header = TRUE)
+head(g5)
+
+g5_ascii <-readLines("/Users/jacktarricone/atms_748/data-code/lab2_data/Lab_2/Group_5/TOA5_21489.SlowResponse_3_2022_01_25_1719.dat") 
+head(g5_ascii)
+
+# remove first unwanted col
+# g5 <- g5[-c(1)]
+# head(g5)
+
+# rename columns using information in the ascii header from g5
+colnames(g5) <- c("year","day","hour_min","sec","BattV_Min","PTemp_C",
+              "AirTC_Avg","RH","windSpeed","windDirection","gustWindSpeed","AirTemp")
+head(g5)
+
+# turn day into dom
+# g5$day <-g5$day -28
+# head(g5) # check, looks good
+# tail(g5)
+
+# add in month column, switch months
+nday_janv1 <-filter(g5, day <= 31)
+nday_jan <-as.integer(nrow(nday_janv1))
+month_jan <-rep(1, nday_jan) 
+month_feb <-rep(2, nrow(g5) - nday_jan)
+month <-c(month_jan, month_feb)
+g5 <-cbind(g5, month) # bind
+
+### add new day col
+# jan
+day_jan <-filter(g5, day <= 31)
+day_jan <-day_jan$day
+
+# feb
+day_feb <-filter(g5, day > 31)
+day_feb <-day_feb$day - 31
+
+# add col
+new_day <-c(day_jan, day_feb)
+tail(new_day)
+g5$new_day <-c(day_jan, day_feb)
+
+# create date col
+date <-mdy(paste0(g5$month,"-",g5$new_day,"-",g5$year))
+g5$date <-date # data to big dataframe
+
+#format time and paste, assuming all second values are 00
+time <- sprintf("%04d", as.numeric(as.character(g5$hour_min)))
+g5$time <-times(gsub("(..)(..)", "\\1:\\2:00", time))
+
+# combine into one date and time columb using lubridate
+g5$date_time <- ymd_hms(paste(g5$date, g5$time))
+head(g5)
+
+# filter off bad data in beginning
+# g5 <-filter(g5, date_time  >= "2022-01-25 17:05:00") 
+
+ggplot(g5) +
+  geom_line(aes(x = date_time, y = RH), size =.6)
+
+# write.csv(g5, "/Users/jacktarricone/atms_748/data-code/lab2_data/csvs/g5.csv")
+
+
+
 
 
